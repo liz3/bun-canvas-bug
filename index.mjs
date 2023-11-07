71aa8cbd3721ee9d093a8d2dcc61f4e9795f5a1e
Liz3 (Yann HN) <accs@liz3.net>
2023-11-07 18:10:20 +0100

    fix

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import { createCanvas } from "canvas";
 const f = () => {
     const canvas = createCanvas(850, 600);
-    // this causes the bug: 
+    // this causes the bug:
     const ctx = canvas.getContext("2d");
     return { buffer: canvas.toBuffer("raw") };
 };
 for (let i = 0; i < 200; i++) {
+    if (process.argv[2] == "gc") Bun.gc(true);
     new Promise((r) => f());
 }
